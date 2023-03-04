e6ad5f188df99b2af196ef5ad372388e3b38d112
MateusSartorio <mateussartorio39@gmail.com>
2023-03-04 13:36:49 -0300

    done for now

--- webapp.go
+++ webapp.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "bufio"
-    "html/template"
-    "log"
-    "net/http"
+	"bufio"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	// "strings"
 )
 
 type ToDoList struct {
@@ -14,6 +15,81 @@ type ToDoList struct {
     Todos []string
 }
 
-func main() {
+func errorCheck(err error) {
+    if err != nil {
+        log.Fatal(err)
+    }
+}
 
+func write(writer http.ResponseWriter, msg string) {
+    _, err := writer.Write([]byte(msg))
+    errorCheck(err)
+}
+
+func englishHandler(writer http.ResponseWriter, request *http.Request) {
+    write(writer, "hello")
+}
+
+func spanishHandler(writer http.ResponseWriter, request *http.Request) {
+    write(writer, "hola")
+}
+
+func getStrings(fileName string) []string {
+    var lines []string
+
+    file, err := os.Open(fileName)
+    if os.IsNotExist(err) {
+        return nil
+    }
+    errorCheck(err)
+    defer file.Close()
+    scanner := bufio.NewScanner(file)
+
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+
+    errorCheck(scanner.Err())
+
+    return lines
+}
+
+func interactHandler(writer http.ResponseWriter, request *http.Request) {
+    toDoVals := getStrings("todos.txt")
+    fmt.Printf("%#v\n", toDoVals)
+    tmpl, err := template.ParseFiles("view.html")
+    errorCheck(err)
+    todos := ToDoList {
+        TodoCount: len(toDoVals),
+        Todos: toDoVals,
+    }
+    err = tmpl.Execute(writer, todos)
+}
+
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+    tmpl, err := template.ParseFiles("new.html")
+    errorCheck(err)
+    err = tmpl.Execute(writer, nil)
+}
+
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+    todo := request.FormValue("todo")
+    options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+    file, err := os.OpenFile("todos.txt", options, os.FileMode(0600))
+    errorCheck(err)
+    _, err = fmt.Println(file, todo)
+    errorCheck(err)
+    err = file.Close()
+    errorCheck(err)
+    http.Redirect(writer, request, "/interact", http.StatusFound)
+}
+
+func main() {
+    http.HandleFunc("/hello", englishHandler)
+    http.HandleFunc("/hola", spanishHandler)
+    http.HandleFunc("/interact", interactHandler)
+    http.HandleFunc("/new", newHandler)
+    http.HandleFunc("/create", createHandler)
+    err := http.ListenAndServe("localhost:8080", nil)
+    log.Fatal(err)
 }
