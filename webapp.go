9793b6611d975351fd101ae42a90d8ac8d4a4a04
MateusSartorio <mateussartorio39@gmail.com>
2023-03-04 11:11:45 -0300

    getting started

---
+++ webapp.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "html/template"
+    "log"
+    "net/http"
+)
+
+type ToDoList struct {
+    TodoCount int
+    Todos []string
+}
+
+func main() {
+
+}
